16c08e3ff9a945d4beb82071e33b02ec76663c88
Christian Bell <christiantcbell@gmail.com>
2018-08-16 10:40:40 +0100

    rails yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: [:index, :show, :find, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+    namespace :admin do
+      resources :restaurants, only: [:edit, :update, :delete]
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Nandos',
+    address: 'Chicken Road',
+    phone_number: "07768996754",
+    category: 'japanese'
+  },
+  {
+    name: 'Pizza Express',
+    address: 'West Road',
+    phone_number: "07768986754",
+    category: 'french'
+  },
+  {
+    name: 'Jamie\'s Italian',
+    address: 'Jamie Road',
+    phone_number: "07768296754",
+    category: 'french'
+  },
+  {
+    name: 'A Pub',
+    address: 'Pub Road',
+    phone_number: "077681996754",
+    category: 'french'
+  },
+  {
+    name: 'Another Restaurant',
+    address: 'food Road',
+    phone_number: "077689963754",
+    category: 'japanese'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
